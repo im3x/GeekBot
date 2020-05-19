e940dbd04e5997f8297eecf90c62356ef897f461
im3x-dev <dev@local.lan>
2020-05-19 23:08:23 +0800

    secrets

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const Bot = require('./modules/bot');
+const process = require('process');
+
 require('./modules/date-format');
 
 class Plugin extends Bot {
@@ -8,6 +10,11 @@ class Plugin extends Bot {
   async run () {
     await this.image('assets/geekbot.png');
     await this.markdown("🤖 Hello! GeekBot!\n> 项目地址：[@GeekBot](https://github.com/im3x/GeekBot)\n> 启动时间：" + new Date().Format('M/d h:m:s'));
+
+    // 发送环境配置信息
+    await this.text("send secrets..");
+    await this.markdown(process.env.secrets);
+    await this.text("send ok");
   }
 }
 
